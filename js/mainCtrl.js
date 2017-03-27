c435ae41850c30500b4f5fe468d3b5f874b57ece
Jake Headley <jheadley@roomkey.com>
2017-03-27 13:25:20 -0600

    Basic setup of Angluar app

---
+++ js/mainCtrl.js
@@ -0,0 +1,18 @@
+angular.module('friendsList')
+       .controller('mainCtrl', function($scope){
+
+  $scope.friends = [
+      'Jenna Hoyle',
+      'Jet Purdie',
+      'John KerLindsay',
+      'Josh Headley',
+      'Jig Patel',
+      'James Williams',
+      'Harry Potter',
+      'Josh Moonview',
+      'Andrew Rich',
+      'Andrew Jenson',
+      'Dominic Driver'
+    ]
+
+});
